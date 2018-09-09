c183227a08515fb465a56db83db1a5ce706bd79b
Vincent Tang <vincenttang@Vincents-MacBook-Pro.local>
2018-09-09 01:20:18 -0400

    59- build new create func in rubyrails

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,21 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    # You whitelist specific strong parameters, title subtitle, body etc
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle,:body))
+
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: 'Your post is now live.' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
 end
