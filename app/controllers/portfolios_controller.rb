2153bc13ab919bd6c942e643970c260f48ffab59
Vincent Tang <vincenttang@Vincents-MacBook-Pro.local>
2018-09-12 23:39:10 -0400

    63 - Building ability to delete items in rails

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,18 @@ class PortfoliosController < ApplicationController
     def show
         @portfolio_item = Portfolio.find(params[:id])
     end
+
+    def destroy
+        # Perform the lookup
+        @portfolio_item = Portfolio.find(params[:id])
+
+        # Destroy/delete the record
+        @portfolio_item.destroy
+
+        # Redirect
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
+          format.json { head :no_content }
+        end
+    end
 end
